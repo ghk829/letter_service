e81a9062dded46a1a6d06b3a39d019a718f167a7
ghk829 <ghk829@hanmail.net>
2019-06-01 21:58:37 +0900

    test

--- s3_client.py
+++ s3_client.py
@@ -1,8 +1,8 @@
 class S3Client:
 
     def __init__(self,**kwargs):
-        self.ACCESS_KEY_ID = kwargs.get("access_key_id","AKIA2UVP3YJ46MD3BZ2Z")
-        self.ACCESS_KEY_PASSWD = kwargs.get("access_key_passwd", "rT8PBBDw32ASlGkAKq1j96I5dLy7I981Jl8ahffE")
+        self.ACCESS_KEY_ID = kwargs.get("access_key_id","AKIAZQQLHJM3VYTL2LWR")
+        self.ACCESS_KEY_PASSWD = kwargs.get("access_key_passwd", "PMLUCrm67HGygjGEOqD0uFna14UlvXOOGTNOM5h1")
         self.BUCKET_NAME = kwargs.get("bucket_name", "glassletter")
         self.REGION_NAME = kwargs.get("region_name","ap-northeast-2")
         self.request = kwargs.get("request")
@@ -21,7 +21,7 @@ class S3Client:
             key 명명 규칙은 source/target/filename
             """
 
-            bucket.put_object(Key=filename, Body=self.request.files)
+            bucket.put_object(Key=filename, Body="testtest")
 
         except Exception as e:
             print(e)
